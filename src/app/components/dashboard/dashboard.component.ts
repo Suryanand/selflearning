72cdf55ad9583811e336baf13c72850f661de522
Suryanand <suryanand000@gmail.com>
2019-11-23 15:06:14 +0530

    json update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -17,21 +17,15 @@ export class DashboardComponent implements OnInit {
   usedCustomIcon: string;
   usedCustomText: string;
   usedTopIconBackground: string;
-  players = [
-    {id:1 , name: 'Sachin', salary: '33000' ,country: 'India', phone : '1234567890' },
-    {id:2 , name: 'Virat', salary: '34000' ,country: 'India', phone : '1234567891'},
-    {id:3 , name: 'Dhoni', salary: '32000' ,country: 'India', phone : '1234567892'},
-    {id:4 , name: 'Yuvi', salary: '21000' ,country: 'India', phone : '1234567893'},
-    {id:5 , name: 'Dravid', salary: '22000' ,country:'India', phone : '1234567894' }
-  ]
+  players : any = data.players;
   constructor(private router: Router, private loginService: LoginService) { }
 
   ngOnInit() {
-    const loggedIn = this.loginService.isLoggedIn;
-    if(!loggedIn){
-      alert("Session expired please login");
-      this.router.navigate(['login']);
-    }
+    // const loggedIn = this.loginService.isLoggedIn;
+    // if(!loggedIn){
+    //   alert("Session expired please login");
+    //   this.router.navigate(['login']);
+    // }
     
     if(this.usedSpaceActual > this.usedSpaceTotal - 5){
        this.usedCustomCssClass = 'material-icons text-success';
